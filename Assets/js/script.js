c3075bd7373bbdc5a358419901f9c1e94e7441ae
Leon <leonwheeler08@gmail.com>
2021-03-24 16:25:10 +0000

    Created an object for a question

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,14 +1,19 @@
+const questions = {
+  question: "When did Javascript first appear?",
+  answer: ["1995", "2000", "1990", "2005"],
+  correctAnswer: "1995",
+};
+
 const remainingTime = document.getElementById("timer");
 const startButton = document.getElementById("startButton");
 let time = 60;
 
 const startTimer = () => {
   remainingTime.innerHTML = time;
+  const countdown = () => {
+    time -= 1;
+  };
+  setInterval(countdown, 1000);
 };
-
-// const countdown = () => {
-//   time -= 1;
-//   setInterval(countdown, 1000);
-// };
 
 startButton.addEventListener("click", startTimer);
