e50e0636adc0f3f84730444004a54801285b42b3
Leon <leonwheeler08@gmail.com>
2021-03-28 18:00:45 +0100

    First place user and score is now displayed.

--- Assets/js/highscores.js
+++ Assets/js/highscores.js
@@ -1,10 +1,21 @@
 const importScores = () => {
   getUsername = localStorage.getItem("username");
+  getScore = localStorage.getItem("highscore");
+  savedScore = JSON.parse(getScore);
+  savedUsername = JSON.parse(getUsername);
+  // for (let index = 0; index < savedHighscores.length; index++) {
   const spanUsername = document.createElement("span");
   spanUsername.classList.add("data");
-  spanUsername.textContent = `${getUsername}`;
-  highscoresDiv.append(spanUsername);
+  spanUsername.textContent = `${savedUsername[0]}`;
+  importData.append(spanUsername);
+
+  const spanScore = document.createElement("span");
+  spanScore.classList.add("data");
+  spanScore.textContent = `${savedScore[0]}`;
+  importData.append(spanScore);
+  // }
 };
 
+const importData = document.getElementById("importedData");
 const highscoresDiv = document.getElementById("highscores");
 highscoresDiv.addEventListener("load", importScores());
